9be1e6cf2bfe13684423fb436e17d2953fee63f7
gnat79 <gnat79@gmail.com>
2018-06-23 15:21:49 -0400

    Show value on active tiles.

--- js/play.js
+++ js/play.js
@@ -2,49 +2,47 @@
 var board = [];
 
 
-$().ready(function() {
+$().ready(function () {
     // Initialize the board to empty.
     for (let j = 0; j < 4; j++) {
         let row = [];
         for (let i = 0; i < 4; i++) {
-            row.push([]);
+            row.push(1);
         }
         board.push(row);
     }
 
-    $("#game_board").on('click', 'div.tile', function() {
-        alert($(this).attr("class"));
-        if ($(this).hasClass("holder")) {
-            alert("holder");
+    $("#game_board").on('click', 'div.tile', function () {
+        if($(this).hasClass("active")) {
+            updateTile($(this));
+        } else {
+            addTile($(this).attr('class'));
         }
-        else if ($(this).hasClass("active")) {
-            alert("active");
-        }
-        else {
-            alert($(this).attr("class"));
-            return false;
-        }
-        // $(this).toggleClass("active holder");
-        addTile($(this).attr('class'));
         return false;
     });
     return false;
 });
 
+function updateTile($tile) {
+    let position = $tile.attr("class");
+    let row = (position.match(/row(\d)/))[1];
+    let col = (position.match(/col(\d)/))[1];
+    $tile.text(board[row][col] *=2);
+}
+
 
 function addTile(position) {
     let row = (position.match(/row(\d)/))[1];
     let col = (position.match(/col(\d)/))[1];
-    alert("Adding a new tile at [" + row + ", " + col + "]");
-    let $newTile = $( '<div class="tile"></div>' );
+    let $newTile = $('<div class="tile"></div>');
     $newTile.addClass(position);
     $newTile.toggleClass("active holder");
     $("#game_board").append($newTile);
-    board[row][col].push($newTile);
-    alert("Success!");
+    board[row][col] *= 2;
+    $newTile.text(board[row][col]);
     return false;
 }
 
-// function setValue(tile, power) {
-//     let color = blue;
-// }
+function setValue(tile, power) {
+    let color = blue;
+}
