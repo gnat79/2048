1e0e576a8c67af09781081c2ac1bbb4ce9797e1c
gnat79 <gnat79@gmail.com>
2018-06-23 11:27:39 -0400

    Minor updates.

--- js/play.js
+++ js/play.js
@@ -1,4 +1,17 @@
+// The game board [row][col]
+var board = [];
+
+
 $().ready(function() {
+    // Initialize the board to empty.
+    for (let j = 0; j < 4; j++) {
+        let row = [];
+        for (let i = 0; i < 4; i++) {
+            row.push([]);
+        }
+        board.push(row);
+    }
+
     $("div.active").click(function() {
         $(this).toggleClass("row1");
         return false;
@@ -12,9 +25,15 @@ $().ready(function() {
 
 
 function addTile(position) {
+    let row = position.match('/row(\d)/');
+    alert(row[0]);
     let $newTile = $( '<div class="tile"></div>' );
     $newTile.addClass(position);
     $newTile.toggleClass("active holder");
     $("#game_board").append($newTile);
     return false;
+}
+
+function setValue(tile, power) {
+    let color = blue;
 }
