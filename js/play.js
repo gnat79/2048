fb78eb3f5b9f9fb255d6a08f7a3484370ec7d6a3
gnat79 <gnat79@gmail.com>
2018-06-23 18:39:07 -0400

    Change the color as the value of the active tile changes.

--- js/play.js
+++ js/play.js
@@ -1,5 +1,21 @@
 // The game board [row][col]
 var board = [];
+var colors = {
+    2: ["#CCCCFF", "black"],
+    4: ["#9999FF", "black"],
+    8: ["#6565FF", "black"],
+    16: ["#3232FF", "black"],
+    32: ["#0000FF", "black"],
+    64: ["#0000CC", "white"],
+    128:["#000099", "white"],
+    256: ["#000066", "white"],
+    512: ["#000032", "white"],
+    1024: ["#000000", "white"],
+    2048: ["#005604", "white"],
+    4096: ["#008906", "black"],
+    8192: ["#FFEE00", "black"],
+    16384: ["#EE00FF", "black"]
+};
 
 
 $().ready(function () {
@@ -7,12 +23,12 @@ $().ready(function () {
     for (let j = 0; j < 4; j++) {
         let row = [];
         for (let i = 0; i < 4; i++) {
-            row.push(1);
+            row.push(0);
         }
         board.push(row);
     }
 
-    $("#game_board").on('click', 'div.tile', function () {
+    $("#game_board").on('mousedown', 'div.tile', function () {
         if($(this).hasClass("active")) {
             updateTile($(this));
         } else {
@@ -27,7 +43,13 @@ function updateTile($tile) {
     let position = $tile.attr("class");
     let row = (position.match(/row(\d)/))[1];
     let col = (position.match(/col(\d)/))[1];
-    $tile.text(board[row][col] *=2);
+    let value = (board[row][col] *= 2);
+    if (value < 16385) {
+        $tile.text(value);
+        $tile.css("background-color", colors[value][0]);
+        $tile.css("color", colors[value][1]);
+    }
+    return false;
 }
 
 
@@ -37,12 +59,8 @@ function addTile(position) {
     let $newTile = $('<div class="tile"></div>');
     $newTile.addClass(position);
     $newTile.toggleClass("active holder");
-    $("#game_board").append($newTile);
-    board[row][col] *= 2;
+    board[row][col] = 2;
     $newTile.text(board[row][col]);
+    $("#game_board").append($newTile);
     return false;
-}
-
-function setValue(tile, power) {
-    let color = blue;
 }
